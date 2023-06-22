7eeb1b35439995a929efdcf772e64f97c3fdc568
avivash <av@andrewvivash.com>
2023-06-22 16:46:33 -0700

    Fix: adapter/utils type errors and jsdoc comments

--- packages/adapter/src/utils.ts
+++ packages/adapter/src/utils.ts
@@ -1,4 +1,10 @@
+/**
+ * Check if MetaMask is installed
+ *
+ * @returns boolean
+ */
 export function hasMetaMask(): boolean {
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
   if (!window.ethereum) {
     return false
   }
@@ -13,38 +19,56 @@ export type GetSnapsResponse = {
     initialPermissions?: { [k: string]: unknown }
   }
 }
+
+/**
+ * Get wallet snaps
+ *
+ * @returns walletSnaps
+ */
 async function getWalletSnaps(): Promise<GetSnapsResponse> {
-  return await window.ethereum.request({
+  return window.ethereum.request({
     method: 'wallet_getSnaps',
   })
 }
 
+/**
+ * Check is MetaMask snaps are supported
+ *
+ * @returns boolean
+ */
 export async function isMetamaskSnapsSupported(): Promise<boolean> {
   try {
     await getWalletSnaps()
     return true
-  } catch (e) {
+  } catch {
     return false
   }
 }
 
 /**
+ * Check if this snap is installed
  *
+ * @param snapOrigin
+ * @param version
  * @returns
  */
 export async function isSnapInstalled(
   snapOrigin: string,
   version?: string
 ): Promise<boolean> {
-  console.log(await getWalletSnaps())
   try {
-    return !!Object.values(await getWalletSnaps()).find(
+    const walletSnaps = await getWalletSnaps()
+    // eslint-disable-next-line no-console
+    console.log('walletSnaps', walletSnaps)
+    const snap = Object.values(walletSnaps).find(
       (permission) =>
         permission.id === snapOrigin &&
-        (!version || permission.version === version)
+        (version === undefined || permission.version === version)
     )
-  } catch (e) {
-    console.log('Failed to obtain installed snaps', e)
+    return Boolean(snap)
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Failed to obtain installed snaps', error)
     return false
   }
 }
