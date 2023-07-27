c9075f803fcfd21badf6a84b6c2ef954bd0c0df3
Agost Biro <5764438+agostbiro@users.noreply.github.com>
2023-07-27 17:19:46 +0200

    docs: add FilForwarder Viem example (#34)
    ---------
    
    Co-authored-by: Hugo Dias <hugomrdias@gmail.com>

---
+++ examples/fil-forwarder-viem/README.md
@@ -0,0 +1,14 @@
+# FilForwarder Viem Example
+
+This example shows how to call the
+[FilForwarder](https://github.com/FILCAT/FilForwarder/tree/main) contract with
+[Viem](https://viem.sh) to transfer FIL from a 0x address to an f1/t1 address.
+
+## Run locally
+
+From this directory:
+
+```bash
+pnpm install
+pnpm dev
+```
---
+++ examples/fil-forwarder-viem/src/main.ts
@@ -0,0 +1,134 @@
+import { Token } from 'iso-filecoin/token'
+import { filForwarderMetadata } from 'filsnap-adapter'
+import {
+  createPublicClient,
+  createWalletClient,
+  custom,
+  getContract,
+  http,
+} from 'viem'
+import { filecoinCalibration } from 'viem/chains'
+import * as Address from 'iso-filecoin/address'
+import { PROTOCOL_INDICATOR } from 'iso-filecoin/address'
+
+/**
+ * Transfer FIL from a 0x address to an f1/t1 address.
+ *
+ * @param recipient - The recipient's address.
+ * @param amount - The amount of FIL to transfer.
+ * @returns The FEVM transaction hash of the transfer.
+ */
+async function transfer(
+  recipient: Address.IAddress,
+  amount: Token
+): Promise<`0x${string}`> {
+  if (recipient.protocol !== PROTOCOL_INDICATOR.SECP256K1) {
+    throw new Error('Recipient must be an f1/t1 address')
+  }
+  if (recipient.network !== 'testnet') {
+    throw new Error('Recipient must be a testnet address')
+  }
+
+  // RPC client for requests that needs to be signed
+  const metaMaskClient = createWalletClient({
+    chain: filecoinCalibration,
+    transport: custom(window.ethereum),
+  })
+  // RPC client for requests that that doesn't need signing
+  const publicClient = createPublicClient({
+    chain: filecoinCalibration,
+    transport: http(),
+  })
+
+  // Prompts user to connect MetaMask
+  const [address] = await metaMaskClient.requestAddresses()
+
+  // Initialize the FilForwarder contract
+  const contract = getContract({
+    address: filForwarderMetadata.contractAddress,
+    abi: filForwarderMetadata.abi,
+    publicClient,
+    walletClient: metaMaskClient,
+  })
+
+  // Call the FilForwarder contract's forward method to make the transfer
+  return await contract.write.forward([recipient.toContractDestination()], {
+    value: amount.toBigInt(),
+    account: address,
+  })
+}
+
+/**
+ * Get the explorer URL for a transaction hash.
+ * Assumes FEVM Calibration test net.
+ *
+ * @param txHash - The recipient's address.
+ * @returns The explorer URL for the transaction.
+ */
+function txHashToExplorerUrl(txHash: `0x${string}`): string {
+  return `https://calibration.filfox.info/en/message/${txHash}`
+}
+
+interface FormElements extends HTMLFormControlsCollection {
+  amount: HTMLInputElement
+  recipient: HTMLInputElement
+}
+
+// False positive
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+const form = document.querySelector('#transfer-form')! as HTMLFormElement
+const txLink = document.querySelector('#tx-link')!
+// False positive
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+const submitButton = document.querySelector(
+  '#submit-button'
+)! as HTMLButtonElement
+
+form.addEventListener('submit', (event) => {
+  // Prevent navigation on submit
+  event.preventDefault()
+
+  const elements = form.elements as FormElements
+
+  // Get the amount to transfer from the form and parse it
+  let recipient
+  try {
+    recipient = Address.from(elements.recipient.value)
+  } catch (error) {
+    console.error(error)
+    alert('Invalid recipient')
+    return
+  }
+
+  // Get the amount to transfer from the form and parse it
+  let amount
+  try {
+    amount = Token.fromFIL(elements.amount.value)
+  } catch (error) {
+    console.error(error)
+    alert('Invalid amount')
+    return
+  }
+
+  // Make the transfer
+  submitButton.disabled = true
+  transfer(recipient, amount)
+    .then((txHash) => {
+      // Insert an anchor tag with the transaction hash
+      const txHashAnchor = document.createElement('a')
+      txHashAnchor.href = txHashToExplorerUrl(txHash)
+      txHashAnchor.textContent = 'View transaction in explorer'
+      txHashAnchor.target = '_blank'
+      // Remove the previous transaction hash anchor
+      txLink.innerHTML = ''
+      // Insert the new transaction hash anchor
+      txLink.append(txHashAnchor)
+    })
+    .catch((error) => {
+      console.error(error)
+      alert('Transfer failed')
+    })
+    .finally(() => {
+      submitButton.disabled = false
+    })
+})
