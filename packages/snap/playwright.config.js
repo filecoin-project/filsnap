d9f07cf117e42e91b9c9af7290efa4e44aea09d9
Hugo Dias <hugomrdias@gmail.com>
2024-06-12 14:10:42 +0100

    chore: disable max failures

--- packages/snap/playwright.config.js
+++ packages/snap/playwright.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
   timeout: process.env.CI ? 60 * 1000 : 30 * 1000,
   fullyParallel: false,
   forbidOnly: Boolean(process.env.CI),
-  maxFailures: process.env.CI ? 1 : 0,
   retries: process.env.CI ? 2 : 1,
   workers: process.env.CI ? 1 : 2,
   reporter: process.env.CI ? [['html'], ['list']] : 'list',
