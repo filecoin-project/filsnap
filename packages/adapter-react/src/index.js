81faada04637d57b142dbc5276c5ae76401388f4
Hugo Dias <hugomrdias@gmail.com>
2024-10-02 17:55:17 +0100

    fix: disconnect when changing to a non filecoin chain

--- packages/adapter-react/src/index.js
+++ packages/adapter-react/src/index.js
@@ -158,6 +158,10 @@ export function FilsnapProvider({
             return
           }
 
+          if (network === undefined) {
+            return await disconnect()
+          }
+
           setIsConfiguring(true)
 
           const data = await snap.changeChain(chainId)
