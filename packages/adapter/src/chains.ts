307658fc001cb20cc33d5d40e3aadfe03d9b832a
Hugo Dias <hugomrdias@gmail.com>
2025-01-29 14:20:10 +0000

    fix: use chain definitions from iso-filecoin

--- packages/adapter/src/chains.ts
+++ packages/adapter/src/chains.ts
@@ -1,27 +1,8 @@
-export const metamask = {
-  mainnet: {
-    chainId: '0x13a',
-    chainName: 'Filecoin - Mainnet',
-    rpcUrls: ['https://api.node.glif.io/rpc/v1'],
-    blockExplorerUrls: ['https://filfox.info', 'https://explorer.glif.io/'],
-    nativeCurrency: {
-      name: 'Filecoin',
-      symbol: 'FIL',
-      decimals: 18,
-    },
-    iconUrls: ['https://filsnap.dev/filecoin-logo.svg'],
-  },
+import { mainnet, testnet, toEthereumChain } from 'iso-filecoin/chains'
 
-  testnet: {
-    chainId: '0x4cb2f',
-    chainName: 'Filecoin - Calibration testnet',
-    rpcUrls: ['https://api.calibration.node.glif.io/rpc/v1'],
-    blockExplorerUrls: ['https://filfox.info', 'https://explorer.glif.io/'],
-    nativeCurrency: {
-      name: 'Filecoin',
-      symbol: 'tFIL',
-      decimals: 18,
-    },
-    iconUrls: ['https://filsnap.dev/filecoin-logo.svg'],
-  },
+export const metamask = {
+  mainnet: toEthereumChain(mainnet),
+  testnet: toEthereumChain(testnet),
 }
+
+export { mainnet, testnet }
