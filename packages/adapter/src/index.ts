53b6bf37fbb4131255354668778c44a9d7fe99d7
Hugo Dias <hugomrdias@gmail.com>
2025-03-10 11:18:44 +0000

    feat!: move iso-filecoin to peer depedencies and remove filforwarder export
    - use filsnap package to get the filforwarder metadata

--- packages/adapter/src/index.ts
+++ packages/adapter/src/index.ts
@@ -2,5 +2,4 @@ export type { SnapConfig, AccountInfo } from 'filsnap'
 export type * from './types'
 
 export { FilsnapAdapter } from './snap'
-export { filForwarderMetadata } from 'filsnap/filforwarder'
 export * from './utils'
